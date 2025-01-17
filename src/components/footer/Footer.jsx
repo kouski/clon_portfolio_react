dd5d556b4b4b0c259e4037552621718127819f74
kouski <72550619+kouski@users.noreply.github.com>
2025-01-17 23:00:41 +0100

    cambio link github

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,7 +7,7 @@ export default function Footer() {
 
     <ul className="logo_list d-flex mb-4 mt-3">
       <li className="ms-3 me-3 list-unstyled">
-        <a href="https://github.com" id="profile-link" target="_blank"><img id="github"
+        <a href="https://github.com/kouski" id="profile-link" target="_blank"><img id="github"
             src="/images/GitHub-Mark-32px.png" alt="Github mark" /></a>
       </li>
       <li className="ms-3 list-unstyled">
